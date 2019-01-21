7104d7cf9519d4219e84995239778c12e0ff2428
unknown <karaansinghallen17@gmail.com>
2019-01-21 23:37:18 +0530

    second

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home ,name="homepage"),
+    path('', views.homepage ,name="homepage"),
 path("countskss/",views.count ,name="count")
 ]
